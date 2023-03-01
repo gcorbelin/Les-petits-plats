8f126f4226e7041f1611a6e99f949f3e0d2f8225
Guillaume Corbelin <guillaume.corbelin@coexya.eu>
2023-03-01 17:11:50 +0100

    refactor(api): use Array functions for API getters

--- src/scripts/api/api.js
+++ src/scripts/api/api.js
@@ -5,92 +5,44 @@ function getDatas() {
 }
 
 function getIngredients(recipes) {
-  let ingredients = [];
+  let allIngredients = [];
   // Check each recipe
-  if (recipes && recipes.length) {
-    for (let i = 0; i < recipes.length; i++) {
-      const recipe = recipes[i];
-      // Check each ingredient
-      if (recipe.ingredients.length) {
-        for (let j = 0; j < recipe.ingredients.length; j++) {
-          const ingredient = recipe.ingredients[j];
-          let ingredientExists = false;
-          // Compare to each ingredient already saved
-          if (ingredients.length) {
-            for (let i = 0; i < ingredients.length; i++) {
-              if (
-                ingredients[i].toLowerCase() ===
-                ingredient.ingredient.toLowerCase()
-              ) {
-                ingredientExists = true;
-              }
-            }
-          }
-          // Add only if not saved
-          if (!ingredientExists) {
-            ingredients.push(ingredient.ingredient.toLowerCase());
-          }
-        }
-      }
-    }
-  }
+  recipes.forEach((recipe) => {
+    const ings = recipe.ingredients;
+    // Check each ingredient
+    ings.forEach((ing) => {
+      // Add ingredient
+      allIngredients.push(ing.ingredient.toLowerCase());
+    });
+  });
+  // Filter duplicates
+  const ingredients = [...new Set(allIngredients)];
 
   return ingredients;
 }
 
 function getAppliances(recipes) {
-  let appliances = [];
+  let allAppliances = [];
   // Check each recipe
-  if (recipes && recipes.length) {
-    for (let i = 0; i < recipes.length; i++) {
-      let applianceExists = false;
-      // Compare to each appliance already saved
-      if (appliances.length) {
-        for (let j = 0; j < appliances.length; j++) {
-          if (
-            appliances[j].toLowerCase() === recipes[i].appliance.toLowerCase()
-          ) {
-            applianceExists = true;
-          }
-        }
-      }
-      // Add only if not saved
-      if (!applianceExists) {
-        appliances.push(recipes[i].appliance.toLowerCase());
-      }
-    }
-  }
+  recipes.forEach((recipe) => {
+    allAppliances.push(recipe.appliance.toLowerCase());
+  });
+  const appliances = [...new Set(allAppliances)];
 
   return appliances;
 }
 
 function getUstensils(recipes) {
-  let ustensils = [];
+  let allUstensils = [];
   // Check each recipe
-  if (recipes && recipes.length) {
-    for (let i = 0; i < recipes.length; i++) {
-      const recipe = recipes[i];
-      // Check each ustensil
-      if (recipe.ustensils.length) {
-        for (let j = 0; j < recipe.ustensils.length; j++) {
-          const ustensil = recipe.ustensils[j];
-          let ustensilExists = false;
-          // Compare it to each ustensil already saved
-          if (ustensils.length) {
-            for (let k = 0; k < ustensils.length; k++) {
-              if (ustensils[k].toLowerCase() === ustensil.toLowerCase()) {
-                ustensilExists = true;
-              }
-            }
-          }
-          // Add only if not saved
-          if (!ustensilExists) {
-            ustensils.push(ustensil.toLowerCase());
-          }
-        }
-      }
-    }
-  }
+  recipes.forEach((recipe) => {
+    const usts = recipe.ustensils;
+    usts.forEach((ust) => {
+      allUstensils.push(ust.toLowerCase());
+    });
+  });
+
+  const ustensils = [...new Set(allUstensils)];
 
   return ustensils;
 }
